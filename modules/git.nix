44812d4213cb842d1ec1210fced66548a0e11aa9
matus.benko <matus.benko@gmail.com>
2022-05-04 23:37:44 +0200

    Update git config

--- modules/git.nix
+++ modules/git.nix
@@ -5,12 +5,34 @@
     git-crypt
     gnupg
     pinentry_qt
+    diff-so-fancy
   ];
 
   programs.git = {
     enable = true;
     userName = "matus.benko";
     userEmail = "matus.benko@gmail.com";
+    aliases = {
+      lgb = "log -n 5 --color=always --pretty='format:%C(yellow)%h%Creset %C(green)%an%Creset %ar%C(red)%d%Creset%n%s%n' --graph";
+    };
+    extraConfig = {
+      core.pager = "diff-so-fancy | less --tabs=4 -RFX";
+      color.ui = true;
+      "color \"diff-highlight\"" = {
+        oldNormal = "red bold";
+        oldHighlight = "red bold 52";
+        newNormal = "green bold";
+        newHighlight = "green bold 22";
+      };
+      "color \"diff\"" = {
+        meta = 11;
+        frag = "magenta bold";
+        commit = "yellow bold";
+        old = "red bold";
+        new = "green bold";
+        whitespace = "red reverse";
+      };
+    };
   };
 
   programs.gpg = {

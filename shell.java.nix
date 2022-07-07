fa38e181199815d543b258cbfc6530d405ca5b9c
matus.benko <matus.benko@gmail.com>
2022-07-07 09:53:28 +0200

    Update java shell

--- shell.java.nix
+++ shell.java.nix
@@ -16,6 +16,8 @@ mkShell {
   ];
   shellHook = ''
     export JAVA_HOME="${jdk11.home}"
+    export ORG_GRADLE_PROJECT_FINAPI_ARTIFACTORY_USER=${workSecrets.artifactoryUser}
+    export ORG_GRADLE_PROJECT_FINAPI_ARTIFACTORY_TOKEN=${workSecrets.artifactoryToken}
     alias gradle="gradle ${workSecrets.gradleProperties} -Dorg.gradle.java.home=${jdk11}/lib/openjdk --no-watch-fs"
   '';
 }

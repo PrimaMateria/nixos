1c77b3e51238d6c9a5a13f6b84875833bacdaa92
matus.benko <matus.benko@gmail.com>
2022-10-13 00:26:22 +0200

    Fix showScratchpad script

--- modules/i3.nix
+++ modules/i3.nix
@@ -46,9 +46,9 @@ let
       i3-msg -t get_tree | \
       jq '.nodes[] | .nodes[] | .nodes[] | select(.name=="__i3_scratch") | .floating_nodes[] | .nodes[] | .window,.name' | \
       sed 's/\"//g' | \
-      paste -s -d ' ' | \
+      paste - - -d ' ' | \
       ${dmenu}/bin/dmenu -nb black -nf white -sb yellow -sf black -l 20 -c | \
-      cut -f1 | \
+      cut -f1 -d ' '| \
       xargs -I "PID" i3-msg "[id=PID] scratchpad show"
      '';
   };

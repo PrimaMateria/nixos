3093c254f12ea254d16500c1ea877d21fbc9b86b
matus.benko <matus.benko@gmail.com>
2023-06-09 11:17:36 +0200

    Add Youtube Ambients flake

---
+++ flakes/youtubeAmbients/flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Youtube Ambients";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+  outputs = inputs@{ self, ... }:
+    let
+      pkgs = import inputs.nixpkgs {
+        system = "x86_64-linux";
+      };
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = with pkgs; [ yt-dlp ffmpeg ];
+        shellHook = ''
+          alias download='yt-dlp https://www.youtube.com/playlist?list=PLjDqZb1FVlst1XPXongf5UD02yU9Md-ot -X'
+          # alias extract='find ./ -exec ffmpeg -i {} -vn -acodec libmp3lame ../mp3/{}.mp3 \;'
+          echo 'ytdl shell'
+        '';
+
+      };
+    };
+}

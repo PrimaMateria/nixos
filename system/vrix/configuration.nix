1cd9ff2605941e2cb2ed5a3520a68935f40d9eed
matus.benko <matus.benko@gmail.com>
2023-06-15 23:31:10 +0200

    introduce vrix wsl system config

---
+++ system/vrix/configuration.nix
@@ -0,0 +1,25 @@
+{ lib, pkgs, config, modulesPath, ... }:
+
+with lib;
+let
+  hostname = "vrix";
+  nixos-wsl = import ../modules/nixos-wsl;
+  core = import ../modules/core.nix {
+    inherit config pkgs hostname;
+  };
+  wsl = import ../modules/wsl.nix {
+    inherit config pkgs hostname;
+  };
+in
+{
+  imports = [
+    "${modulesPath}/profiles/minimal.nix"
+    nixos-wsl.nixosModules.wsl
+    core
+    wsl
+  ];
+
+  # services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.opengl.enable = true;
+
+}

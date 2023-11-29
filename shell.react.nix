cbe1f7335ffadd718d1e7c94cecc18a7266abf25
matus.benko <matus.benko@gmail.com>
2023-11-29 08:43:33 +0100

    Simplify react shell

--- shell.react.nix
+++ shell.react.nix
@@ -11,20 +11,6 @@ let
   
     ${workSecrets.internalNpmRegistry}
   '';
-  nixos-playwright = stdenv.mkDerivation {
-    pname = "nixos-playwright";
-    version = "0.0.1";
-    src = fetchgit {
-      url = "https://github.com/ludios/nixos-playwright";
-      sha256 = "1yb4dx67x3qxs2842hxhhlqb0knvz6ib2fmws50aid9mzaxbl0w0";
-      rev = "fdafd9d4e0e76bac9283c35a81c7c0481a8b1313";
-    };
-    phases = [ "installPhase" ];
-    installPhase = ''
-      mkdir -p $out/bin
-      cd $out/bin && cp $src/* .
-    '';
-  };
 
   testdebug = pkgs.writeShellApplication {
     name = "testdebug";
@@ -45,13 +31,6 @@ mkShell {
   name = "react-shell";
   buildInputs = [
     unstable.nodejs-18_x
-    unstable.mockoon
-
-    # needed for playwright
-    unstable.google-chrome-dev
-    firefox-bin
-    nixos-playwright
-
     testdebug
   ];
   shellHook = ''
@@ -65,12 +44,5 @@ mkShell {
     export PATH="$HOME/.npm-global/bin:$PATH"
 
     export DISPLAY=:1
-    export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
-
-    playwright-install () {
-      #npm install -D playwright
-      npx playwright install
-      fix-playwright-browsers
-    }
   '';
 }
